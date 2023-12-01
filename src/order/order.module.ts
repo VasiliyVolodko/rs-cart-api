189fe13ea5041f90fbb57be13721b278c77127fb
Vasiliy Volodko <Vasiliy_Volodko@epam.com>
2023-12-01 12:26:45 +0100

    task-8

--- src/order/order.module.ts
+++ src/order/order.module.ts
@@ -1,7 +1,10 @@
 import { Module } from '@nestjs/common';
 import { OrderService } from './services';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Orders } from 'src/cart/services/cart.entities';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Orders])],
   providers: [ OrderService ],
   exports: [ OrderService ]
 })
--- src/order/services/order.service.ts
+++ src/order/services/order.service.ts
@@ -1,39 +1,49 @@
 import { Injectable } from '@nestjs/common';
 import { v4 } from 'uuid';
 
-import { Order } from '../models';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Orders } from 'src/cart/services/cart.entities';
+import { Repository } from 'typeorm';
 
 @Injectable()
 export class OrderService {
-  private orders: Record<string, Order> = {}
+  constructor(
+    @InjectRepository(Orders)
+    private ordersRepository: Repository<Orders>,
+  ) { }
 
-  findById(orderId: string): Order {
-    return this.orders[ orderId ];
+  async findById(orderId: string): Promise<Orders> {
+    return await this.ordersRepository.findOne({
+      where: {
+        id: orderId
+      },
+      relations: {
+        cart: true
+      }
+    })
   }
 
-  create(data: any) {
+  async create(data: any) {
     const id = v4(v4())
-    const order = {
+
+    await this.ordersRepository.insert({
       ...data,
       id,
       status: 'inProgress',
-    };
+    })
 
-    this.orders[ id ] = order;
-
-    return order;
+    return await this.findById(id)
   }
 
-  update(orderId, data) {
-    const order = this.findById(orderId);
+  async update(orderId, data) {
+    const order = await this.findById(orderId);
 
     if (!order) {
       throw new Error('Order does not exist.');
     }
-
-    this.orders[ orderId ] = {
+    await this.ordersRepository.update({ id: orderId }, {
       ...data,
       id: orderId,
-    }
+    })
   }
 }
